016b1aac35fab0bf94c10b790c0414b97e9be0cd
diyousee <ruhuzaduc@gmail.com>
2023-05-02 18:05:34 +0200

    Changing s1 and s2

--- javascript/js/homework.js
+++ javascript/js/homework.js
@@ -16,7 +16,7 @@ function arithmeticResult(){
 function equationSecondDegre(){    
     document.getElementById("result").innerHTML = "";
 
-    let a = 1; let b = 20; let c = 30;
+    let a = -4; let b = -3; let c = 10;
     let delta = b**2-4*a*c;
 
     console.log(delta);
@@ -24,16 +24,18 @@ function equationSecondDegre(){
     let abcData = "<p>Si a = "+a+" ; b = "+b+" ; c = "+c+"</p>";    
     let equation = a+"x<sup>2</sup> + "+b+"x + "+c;
 
+    console.log(Math.sqrt(delta));
+
     if(delta>0){
-        var s1 = (-b-Math.sqrt(delta))/2*a;
-        var s2 = (-b+Math.sqrt(delta))/2*a;
+        var s1 = (-b - Math.sqrt(delta))/(2*a);
+        var s2 = (-b + Math.sqrt(delta))/(2*a);
         
         let result = "X<sub>1</sub> = "+s1+" , X<sub>2</sub> = "+s2;
 
         document.getElementById("result").innerHTML = 
         "<p>Equation : ax<sup>2</sup> + bx + c</p> <p>"+abcData+"</p> <p>Delta b<sup>2</sup> - 4ac = "+delta+"</p> <p class='finalResult'>Results : "+equation+" <h3>"+result+"</h3></p>";
     }else{
-        document.getElementById("result").innerHTML = "<p>Equation : ax<sup>2</sup> + bx + c</p> <p>"+abcData+"</p> <p class='finalResult'>Pas de solution car Delta = b<sup>2</sup> - 4ac est negatif</p>"
+        document.getElementById("result").innerHTML = "<p>Equation : ax<sup>2</sup> + bx + c</p> <p>"+abcData+"</p> <p class='finalResult'>Pas de solution car Delta = b<sup>2</sup> - 4ac = "+delta+" est negatif</p>"
     }
 
 
